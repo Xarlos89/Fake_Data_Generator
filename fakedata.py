61a3c9da260151104873bd4baa2267d94937bd91
Xarlos <Aartizada@gmail.com>
2022-04-07 07:23:08 +0200

    Commenting and beautifying

--- fakedata.py
+++ fakedata.py
@@ -1,20 +1,28 @@
 from faker import Faker
+#### #### #### #### 
+#### For other locals, see the Docs. 
+#### https://faker.readthedocs.io/en/master/locales.html
 
 
+#### Set the Local preferences
 # fake = Faker(['de_AT']) # Austrians
 fake = Faker(['en_US']) # Americans
+# fake = Faker(['ja-JP']) # Japanese
+# fake = Faker(['en_GB']) # British
+
+#### Apply variables
 firstName = fake.first_name()
 lastName = fake.last_name()
 email = fake.ascii_email()
-# houseNumber = fake.builidng_number()
+houseNumber = fake.builidng_number()
 street = fake.street_name()
 postalCode = fake.postcode()
 city = fake.city()
 phone = fake.msisdn()
 socialNumber = fake.ssn()
 
-
-print('Name: {} {}\nEmail: {}\nStreet: {}\nCity: {} {}\nPhone Number: {}\nSocial Security Number: {}\n'.format(firstName,lastName,email,street,city,postalCode,phone,socialNumber))
+# Pretty print a fake person
+print('Name: {} {}\nEmail: {}\nStreet: {}\nHouse Number: {}\nCity: {} {}\nPhone Number: {}\nSocial Security Number: {}\n'.format(firstName,lastName,email,street,houseNumber,city,postalCode,phone,socialNumber))
 
 
 
